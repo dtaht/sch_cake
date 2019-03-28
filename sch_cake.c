755ba4cda56bec45d20f7928f1442fdf83c12573
Jonathan Morton <chromatix99@gmail.com>
2019-03-29 00:30:19 +0100

    Make the ramp shallower, but still shifted up.

--- sch_cake.c
+++ sch_cake.c
@@ -599,7 +599,7 @@ static bool cobalt_should_drop(struct cobalt_vars *vars,
 
 	/* Simple SCE probability ramp from zero to target delay. */
 	if (is_bulk && sojourn > (p->target/2) && p->inv_target) {
-		if(over_target || prandom_u32() < (sojourn - p->target/2) * 2 * p->inv_target)
+		if(over_target || prandom_u32() < (sojourn - p->target/2) * (p->inv_target/2))
 			vars->sce_marked = INET_ECN_set_sce(skb);
 	}
 
