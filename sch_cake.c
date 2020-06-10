7d7e92caaa2ad60f9fed9041126cf28363eca7f4
Ilya Ponetayev <i.ponetaev@ndmsystems.com>
2020-06-10 18:03:29 +0300

    sch_cake: properly handle skb protocol on CE
    cobalt_should_drop() tries to set CE via INET_ECN_set_ce() call, but this
    function falsely relies on skb->protocol to determine network header. Fix
    this by implementing cobalt_set_ce() helper and employ cake_skb_proto() for
    proper detection.
    
    Fixes: 3fb9914cec ("Retire usage of cobalt.c and merge cobalt functionality elsewhere")
    Fixes: 6cc417265c ("Add COBALT core algorithm - combination of simplfied Codel and BLUE.")
    Signed-off-by: Ilya Ponetayev <i.ponetaev@ndmsystems.com>
    [reflow commit message, rename cobalt_set_ce() to cake_set_ce()]
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- sch_cake.c
+++ sch_cake.c
@@ -508,6 +508,52 @@ static bool cobalt_queue_empty(struct cobalt_vars *vars,
 	return down;
 }
 
+static __be16 cake_skb_proto(const struct sk_buff *skb)
+{
+	unsigned int offset = skb_mac_offset(skb) + sizeof(struct ethhdr);
+	__be16 proto = skb->protocol;
+	struct vlan_hdr vhdr, *vh;
+
+	while (proto == htons(ETH_P_8021Q) || proto == htons(ETH_P_8021AD)) {
+		vh = skb_header_pointer(skb, offset, sizeof(vhdr), &vhdr);
+		if (!vh)
+			break;
+
+		proto = vh->h_vlan_encapsulated_proto;
+		offset += sizeof(vhdr);
+	}
+
+	return proto;
+}
+
+static int cake_set_ce(struct sk_buff *skb)
+{
+	int wlen = skb_network_offset(skb);
+
+	switch (cake_skb_proto(skb)) {
+	case htons(ETH_P_IP):
+		wlen += sizeof(struct iphdr);
+		if (!pskb_may_pull(skb, wlen) ||
+		    skb_try_make_writable(skb, wlen))
+			return 0;
+
+		return IP_ECN_set_ce(ip_hdr(skb));
+
+	case htons(ETH_P_IPV6):
+		wlen += sizeof(struct ipv6hdr);
+		if (!pskb_may_pull(skb, wlen) ||
+		    skb_try_make_writable(skb, wlen))
+			return 0;
+
+		return IP6_ECN_set_ce(skb, ipv6_hdr(skb));
+
+	default:
+		return 0;
+	}
+
+	return 0;
+}
+
 /* Call this with a freshly dequeued packet for possible congestion marking.
  * Returns true as an instruction to drop the packet, false for delivery.
  */
@@ -560,7 +606,7 @@ static bool cobalt_should_drop(struct cobalt_vars *vars,
 
 	if (next_due && vars->dropping) {
 		/* Use ECN mark if possible, otherwise drop */
-		drop = !(vars->ecn_marked = INET_ECN_set_ce(skb));
+		drop = !(vars->ecn_marked = cake_set_ce(skb));
 
 		vars->count++;
 		if (!vars->count)
@@ -593,24 +639,6 @@ static bool cobalt_should_drop(struct cobalt_vars *vars,
 		vars->drop_next = now;
 
 	return drop;
-}
-
-static __be16 cake_skb_proto(const struct sk_buff *skb)
-{
-	unsigned int offset = skb_mac_offset(skb) + sizeof(struct ethhdr);
-	__be16 proto = skb->protocol;
-	struct vlan_hdr vhdr, *vh;
-
-	while (proto == htons(ETH_P_8021Q) || proto == htons(ETH_P_8021AD)) {
-		vh = skb_header_pointer(skb, offset, sizeof(vhdr), &vhdr);
-		if (!vh)
-			break;
-
-		proto = vh->h_vlan_encapsulated_proto;
-		offset += sizeof(vhdr);
-	}
-
-	return proto;
 }
 
 #if IS_REACHABLE(CONFIG_NF_CONNTRACK)
