ee3fe0570b7ee0ce628dcd31a8f7776446f57463
Aaron U'Ren <aauren@users.noreply.github.com>
2020-11-12 14:07:47 -0600

    sch_cake: remove deprecated uninitialized_var function

--- sch_cake.c
+++ sch_cake.c
@@ -1809,7 +1809,7 @@ static s32 cake_enqueue(struct sk_buff *skb, struct Qdisc *sch,
 {
 	struct cake_sched_data *q = qdisc_priv(sch);
 	int len = qdisc_pkt_len(skb);
-	int uninitialized_var(ret);
+	int ret = 0;
 	struct sk_buff *ack = NULL;
 	ktime_t now = ktime_get();
 	struct cake_tin_data *b;
